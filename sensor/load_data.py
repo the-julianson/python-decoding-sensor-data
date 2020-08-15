b71bfec195a4de9b9e218b346594fcdf4bc6f5ff
Julian Cuentas <juliancuentas@hotmail.com>
2020-08-15 19:22:15 -0300

    solved a probblem

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -3,7 +3,7 @@ import os, glob, csv
 #Create a function to parse the data
 def load_sensor_data():
     sensor_data = []
-    sensor_files = glob.glob(os.path.join(os.getcwd(),'datasets', "*csv"))
+    sensor_files = glob.glob(os.path.join(os.getcwd(),'datasets', "*.csv"))
 
     for sensor_file in sensor_files:
         with open(sensor_file) as data_file:
@@ -11,5 +11,5 @@ def load_sensor_data():
 
             for row in data_reader:
                 sensor_data.append(row)
-    print(sensor_data)
+    
     return sensor_data
