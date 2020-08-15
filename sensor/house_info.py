a6e75a212b2d6dc625153b466b4278802a2ab236
Julian Cuentas <juliancuentas@hotmail.com>
2020-08-15 19:59:03 -0300

    minor change

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -18,7 +18,7 @@ class HouseInfo:
     def get_data_by_date(self, field, rec_date=date.today()):
         field_data = []
         for record in self.data:
-            if rec_area == strtftime(record['date']):
+            if rec_date == strtftime(record['date']):
                 field_data.append(record['field'])
         return field_data 
 
