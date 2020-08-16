6866502a24e92e994b2d8783bb67c90ff64ca454
Julian Cuentas <juliancuentas@hotmail.com>
2020-08-16 14:56:22 -0300

    sensor

---
+++ sensor/energy_info.py
@@ -0,0 +1,33 @@
+from .house_info import HouseInfo
+
+from datetime import date
+
+
+class EnergyData(HouseInfo):
+    ENERGY_PER_BULB = 0.2
+    ENERGY_BITS = 0x0F0
+
+    def _get_energy(self, rec):
+        # Convert rec hexadecimal to integer
+        energy = int(rec, base=16)
+        energy = energy & self.ENERGY_BITS          # Mask Energy bits
+        energy = energy >> 4                        # shift right
+        return energy
+
+    def _convert_data(self, data):
+        recs = []
+        for rec in data:
+            # Convert string of hex into actual integers based 10
+            recs.append(self._get_energy(rec))
+        return recs
+
+    def get_data_by_date(self, rec_date=date.today()):
+        recs = super().get_data_by_date('energy_usage', rec_date)
+        return self._convert_data(recs)
+
+    def get_data_by_area(self, rec_area=0):
+        recs = super().get_data_by_area('energy_usage', rec_area)
+        return self._convert_data(recs)
+
+
+
